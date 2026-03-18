e5d4f1c7c7e3b288a219cc3091ece0747129fb44
NATHAN PROENÇA DE LUNA <25205657@cfp127aluno.local>
2026-03-18 16:56:59 -0300

    iniciando

---
+++ função.js
@@ -0,0 +1,15 @@
+const BR = require("./estados_cidades.js");
+
+const getListaDeEstados = function(){
+    
+
+    seiLa.forEach(function(item){
+        console.log(item)
+    })
+
+}
+
+getListaDeEstados()
+
+
+
