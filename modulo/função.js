728307fca6a83788700206406a8d9d3ca2ab4a53
NathanLuna15 <25205657@cfp127aluno.local>
2026-03-25 16:32:40 -0300

    finalizado

--- modulo/função.js
+++ modulo/função.js
@@ -53,11 +53,73 @@ const getCapitalEstado = function(siglaStados){
 }
 
 const getEstadosRegiao = function(regiao){
-    let 
+    let regiaoEscolhida = String(regiao).toUpperCase()
+    let uf = {
+        "regiao": regiaoEscolhida.toUpperCase(),
+        "estados": []
+    }
+    for (let estado of listaDeEstados) {
+        if (regiaoEscolhida == String(estado.regiao).toUpperCase()) {
+            uf.estados.push({
+                "uf": estado.sigla, 
+                "descricao": estado.nome
+            })
+        }
+    }
+
+    if (uf.estados.length === 0){
+        return false
+    }else{
+        return uf
+    }
 }
 
-//getListaDeEstados()
-//console.log(getDadosEstado("rj"))
-console.log(getCapitalEstado("ac"))
+const getCapitalPais = function () {
+    let capitaisPais = {
+        "capitais": []
+    }
 
+    listaDeEstados.forEach(function (pegarCapitais) {
+        if (pegarCapitais.capital_pais) {
+            capitaisPais.capitais.push({
+                "capital_atual": pegarCapitais.capital_pais.capital,
+                "uf": pegarCapitais.sigla,
+                "descricao": pegarCapitais.nome,
+                "capital": pegarCapitais.capital,
+                "regiao": pegarCapitais.regiao,
+                "capital_pais_ano_inicio": pegarCapitais.capital_pais.ano_inicio,
+                "capital_pais_ano_termino": pegarCapitais.capital_pais.ano_fim
+            })
+        }
+    })
 
+    return capitaisPais
+}
+
+const getCidades = function (cidadesSigla) {
+    let siglaInf = String(cidadesSigla).toUpperCase()
+    let infoCidades
+    let cidades = []
+
+    for (let sigla of listaDeEstados) {
+        if (siglaInf == String(sigla.sigla).toUpperCase()) {
+            sigla.cidades.forEach(function (todasCidades) {
+                cidades.push(todasCidades.nome)
+
+                infoCidades = {
+                    "uf": sigla.sigla,
+                    "descricao": sigla.nome,
+                    "quantidade_cidades": sigla.cidades.length,
+                    "cidades": cidades
+                }
+            })
+        }
+    }
+
+    if (cidades.length === 0)
+        return false
+
+    return infoCidades
+}
+
+console.log(getCidades("sp"))
